06db328429aa0736e625304180b87601b5480a67
Darthsoviet <31465914+Darthsoviet@users.noreply.github.com>
2019-10-09 13:34:39 -0500

    Update Suma-fila.py
    Corr

--- Suma-fila.py
+++ Suma-fila.py
@@ -1,14 +1,16 @@
 def Suma(lista):
 
     suma=lista[0]
+    lista2=lista[1:]
     if len(lista)==1:
         return suma
     else:
-        return suma+Suma(lista[1:])
+
+        return suma+Suma(lista2)
 
 def main():
     print(“—————————————-“)
-    lista=[,4,2,5,6,2,4]
-    print(f”{suma_lista(l)}”)
+    lista=[4,2,5,6,2,4]
+    print(f”{suma_lista(lista)}”)
 
 main()
